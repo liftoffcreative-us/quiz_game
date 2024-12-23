f43e1fdeaf62f03ed20cbdcdee8cbdcb7405c582
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2024-12-23 16:04:21 -0600

    label

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -11,7 +11,7 @@ const Timer = ({questionAnswered, setQuestionAnswered}) => {
     const initialTime = 20 
     const [timeRemaining, setTimeRemaining] = useState(initialTime)
     
-
+// Handle timer countdown
     useEffect( () => {
         const timerInterval = setInterval(() => {
             if (questionAnswered ==false){
