f401dd755e10aababc27c47d160a77e1cbe1aaa3
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2024-12-23 17:31:44 -0600

    updated home page and routed categories page

---
+++ app/categories/page.js
@@ -0,0 +1,10 @@
+
+import Categories from "./Categories";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Categories />
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Questions from "./components/Questions";
 export default function Home() {
   return (
     <div className="flex items-center justify-center w-screen h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Questions />
+      <a href="/categories"><button className="flex items-center justify-center w-auto px-6 py-2 bg-game-blue rounded-lg">CATEGORIES</button></a>
     </div>
   );
 }
