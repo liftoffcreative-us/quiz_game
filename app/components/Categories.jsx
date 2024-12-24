5a2c40a76e64dd72fc758fb3630bf9a3a67b1759
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2024-12-24 15:17:02 -0600

    Category coloring

--- app/components/Categories.jsx
+++ app/components/Categories.jsx
@@ -13,16 +13,49 @@ const Categories = ({ allCategories }) => {
     <div className='flex flex-col items-center justify-center w-3/4 h-3/4 bg-gray-900 rounded-xl'>
       <h1 className='my-6 text-4xl'>CATEGORIES</h1>
       <div className='flex justify-center items-start flex-wrap gap-6 w-[100%] h-full overflow-y-auto'>
-        { allCategories.slice(0,16).map((category, index) => {
+        { allCategories.slice(0,18).map((category, index) => {
           const idNum = category.id
-          
-          console
-
+          // Look at the index of the category to determine a color to associate with it (we can pass the "color_name" value to the ring question to determine which ring to mark as "completed"). 
+          const colors = [
+            {
+              color_name: "game-purple",
+              colorHex: '#862dba',
+              colorIds: [0,6,12,18]
+            },
+            {
+              color_name: "game-blue",
+              colorHex: '#00b3ff',
+              colorIds: [1,7,13,19]
+            },
+            {
+              color_name: "game-red",
+              colorHex: '#cf1120',
+              colorIds: [2,8,14,20]
+            },
+            {
+              color_name: "game-orange",
+              colorHex: '#ed8c2b',
+              colorIds: [3,9,15,21]
+            },
+            {
+              color_name: "game-green",
+              colorHex: '#3eb53e',
+              colorIds: [4,10,16,22]
+            },
+            {
+              color_name: "game-yellow",
+              colorHex: '#ffe600',
+              colorIds: [5,11,17,23]
+            },            
+          ]
+          const setColor = (colors.filter((color) => color.colorIds.includes(index)))
+          const colorCode = (setColor[0].colorHex)
+          console.log(colorCode)
           return(
             <Link key={index} href="/questions" className='border border-white flex flex-col items-center justify-center w-1/5 h-1/3 rounded-lg overflow-hidden'>
        
             <Image src={`/${idNum.toString()}.jpg`} alt="Category Image" width={1390} height={900} className='w-full h-[75%] bg-cover bg-right' />
-            <div className='w-full h-[25%] flex items-center justify-center bg-blue-900 font-bold text-[.7rem] text-center'>{category.name}</div>
+            <div style={{'--colorHex': colorCode}} className='bg-[var(--colorHex)] w-full h-[25%] flex items-center justify-center font-bold text-[.7rem] text-center'>{category.name}</div>
          
             </Link>
           )
