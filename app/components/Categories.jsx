1231f9f273ebd0db6a080db62b63685fa6ab16a4
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2024-12-24 12:32:28 -0600

    update categories

--- app/components/Categories.jsx
+++ app/components/Categories.jsx
@@ -13,7 +13,7 @@ const Categories = ({ allCategories }) => {
     <div className='flex flex-col items-center justify-center w-3/4 h-3/4 bg-gray-900 rounded-xl'>
       <h1 className='my-6 text-4xl'>CATEGORIES</h1>
       <div className='flex justify-center items-start flex-wrap gap-6 w-[100%] h-full overflow-y-auto'>
-        { allCategories.slice(0,8).map((category, index) => {
+        { allCategories.slice(0,16).map((category, index) => {
           const idNum = category.id
           
           console
--- app/components/Questions.jsx
+++ app/components/Questions.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import Timer from './Timer'
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Questions = () => {
@@ -11,6 +12,7 @@ const Questions = () => {
     const [isLoading, setLoading] = useState(true)
     const [selected, setSelected] = useState(null)
     const [sortedAnswers, setSortedAnswers] = useState([])
+    const [randomNumber, setRandomNumber] = useState()
 
     useEffect(() => {
         fetch('/api/rand-question')
@@ -18,20 +20,21 @@ const Questions = () => {
           .then((questionData) => {
             setQuestionData(questionData)
             setLoading(false)
-            setSortedAnswers(questionData.data.answers.sort(() => Math.random() - 0.5))
+            setRandomNumber(Math.random() - 0.5)
           })
+          
       }, [])
      
       if (isLoading) return <p>Loading...</p>
       if (!questionData) return <p>No profile data</p>
-
+      setSortedAnswers(questionData.data.answers.sort(() =>randomNumber))
     const answerChoice = (index) => {
         setQuestionAnswered(true)
         setSelected(index)
         
         
     }
-   
+   console.log(questionData.data.question)
 
   return (
     
@@ -43,7 +46,7 @@ const Questions = () => {
             <div className='absolute w-1/2 h-1/2 bg-times-up-bg bg-contain bg-no-repeat '></div>
         </Link>
         <section id="topSection" className='flex items-center w-full h-1/2  overflow-hidden'>
-         <div className='mt-4 ml-2 bg-question-bg bg-cover w-[55%] h-full rounded-md '></div>
+         <Image src="/9.jpg" alt="Category Image"width={1300} height={900} className='mt-4 ml-2 bg-question-bg bg-cover w-[55%] h-full rounded-md ' />
         
         </section>
         <section id="bottomSection" className='flex w-full h-1/2 '>
@@ -51,7 +54,7 @@ const Questions = () => {
                 <Timer questionAnswered={questionAnswered} />
             </div>
             <div id="questionSection" className='flex flex-col items-start justify-center w-3/4 h-full p-6'>
-                <div id="question" className='text-2xl w-full h-1/3 '>{questionData.data.question.includes('&quot;')? questionData.data.question.replaceAll('&quot;', '"') : questionData.data.question}</div>
+                <div id="question" className='text-2xl w-full h-1/3 '>{questionData.data.question}</div>
                 <div className='flex flex-wrap w-full h-2/3 gap-4'>
                 { sortedAnswers.map((answer, index) => {
                 const letter = ["A", "B", "C", "D"]
