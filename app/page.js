15f5117e36145e916d016269ea8424b1811245b3
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2024-12-29 14:19:49 -0600

    cosmetic

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-12 w-screen h-screen bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-grad-lt-blue to-grad-dk-blue">
+    <div className="flex flex-col items-center justify-center gap-12 w-screen h-screen bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-grad-lt-blue to-game-bg">
       <Image src="/logoMain.png" alt="Main Logo" width={1090} height={560} className='w-1/3'/>
       <Link href="/add-player" className='flex items-center justify-center'>
         <Image src='/startBtn.svg' width={600} height={200} alt="Start Button" className='w-[80%]'/>
