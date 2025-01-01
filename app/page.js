a95eb60d6bf69d78db2e12702d58675bb27f1e88
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-01-01 01:58:38 -0600

    additional logo updates

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-12 w-screen h-screen bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-grad-lt-blue to-game-bg">
-      <Image src="/logoMain.png" alt="Main Logo" width={1090} height={560} className='w-[90%] md:w-[50%]'/>
+      <Image src="/logoMain.svg" alt="Main Logo" width={1090} height={560} className='w-[90%] md:w-[50%]'/>
       <Link href="/add-player" className='flex items-center justify-center'>
         <Image src='/startBtn.svg' width={600} height={200} alt="Start Button" className='w-[80%]'/>
       </Link>
